f5524b540081ed9393259388e22fc30005f0ef04
Felipe Nader <fsnader@gmail.com>
2023-04-13 20:29:20 -0300

    Save

--- backend/src/infrastructure/repositories/ordersRepository.js
+++ backend/src/infrastructure/repositories/ordersRepository.js
@@ -1,7 +1,16 @@
+const fs = require('fs/promises');
+
+const appendToFile = async (order) => {
+  await fs.appendFile(
+    'data/received-orders.json',
+    `${JSON.stringify(order)}\n`);
+};
+
 async function saveOrder(order) {
   // Here we would call a database to save the order.
-  // This logic is being simplified here by this console.log() call
+  // This logic is being simplified here by saving it to data/received-orders.json
   console.log(`Saving order in the database`, order);
+  await appendToFile(order);
 }
 
 module.exports = {
